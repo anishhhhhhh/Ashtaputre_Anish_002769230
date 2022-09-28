a086a6695e4f9ddd2d28b9fe02c6a0515c9a79c8
Anish <Anish@10.110.115.26>
2022-09-28 17:47:06 -0400

    Add Create Employee UI

---
+++ src/Model/EmployeeDetails.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Anish
+ */
+public class EmployeeDetails {
+    private String name;
+    private int employeeID;
+    private int age;
+    private String gender;
+    private String startDate;
+    private int level;
+    private String teamInfo;
+    private String positionTitle;
+    private String contactInfo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getTeamInfo() {
+        return teamInfo;
+    }
+
+    public void setTeamInfo(String teamInfo) {
+        this.teamInfo = teamInfo;
+    }
+
+    public String getPositionTitle() {
+        return positionTitle;
+    }
+
+    public void setPositionTitle(String positionTitle) {
+        this.positionTitle = positionTitle;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+    
+}
---
+++ src/Model/EmployeeDetailsHistory.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Anish
+ */
+public class EmployeeDetailsHistory {
+    
+    private ArrayList<EmployeeDetails> tableData;
+    
+    //constructer(this method will always be called when called EmployeeDetailsHistory)
+    public EmployeeDetailsHistory(){
+        this.tableData = new ArrayList<EmployeeDetails>();
+    }
+
+    public ArrayList<EmployeeDetails> getTableData() {
+        return tableData;
+    }
+
+    public void setTableData(ArrayList<EmployeeDetails> tableData) {
+        this.tableData = tableData;
+    }
+}
